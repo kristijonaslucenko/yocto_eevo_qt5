dc1a0926e903744e78ac44d73f718533d135f561
Kristijonas Lucenko <bigrun123@gmail.com>
2016-03-23 10:00:06 +0100

    production flashing script is added

---
+++ tools/production_flashing/flash_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+convertsecs() {
+ ((h=${1}/3600))
+ ((m=(${1}%3600)/60))
+ ((s=${1}%60))
+ printf "%02d:%02d:%02d\n" $h $m $s
+}
+
+start=$SECONDS
+minicom -D /dev/ttyUSB0 -S sx
+duration=$(( SECONDS - start ))
+echo "Time elapsed: " $(convertsecs $duration)
